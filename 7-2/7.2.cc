5e11991f411eff3d2460f867d2089dc53cebf8a6
mueller-seb <mueller.sbstn@web.de>
2014-12-07 13:59:18 +0100

    Änerung Gergana

--- 7-2/7.2.cc
+++ 7-2/7.2.cc
@@ -1,3 +1,5 @@
+#include "fcpp.hh"
+
 // Ein Listenelement
 struct IntListElem {
   IntListElem* next;  // Zeiger auf nächstes Element
@@ -65,67 +67,103 @@ IntListElem* remove_from_list (IntList* list, IntListElem* where)
 
 IntListElem* durchlaufe_liste(IntList liste, int j)
 {
-IntListElem* p = liste.first;
-	for (int i=0; i < j; i++)
-		p=p->next;
-	return p;
+ IntListElem* p = liste.first;
+ for (int i=0; i < j; i++)
+  p=p->next;
+ return p;
 }
 
-IntList* ZyklusListe(int k, int n, int m)
+IntListElem* ZyklusListe(int k, int n, int m)
 {
-	IntList l_linear;
-	IntList l_zyklus;
+  IntList l_linear;
+  IntList l_zyklus;
+  empty_list(&l_linear);
+  empty_list(&l_zyklus);
 
-	empty_list(&l_linear);
-	empty_list(&l_zyklus);
+  if(k==0&&n==0) return l_linear.first;
 
-	IntListElem* l_zyklus_last = new IntListElem;
-	IntListElem* l_linear_last = new IntListElem;
+  if(k=0&&n!=0)
+  {
+     IntListElem* l_zyklus_last = new IntListElem;
+     insert_in_list(&l_zyklus, 0, l_zyklus_last);
 
-	insert_in_list(&l_zyklus, 0, l_zyklus_last);
-	insert_in_list(&l_linear, 0, l_linear_last);
+     for (int i=1; i<n; i++)
+       insert_in_list(&l_zyklus, 0, new IntListElem);
+     l_zyklus_last->next = l_zyklus.first;
 
+     l_linear.first= durchlaufe_liste(l_zyklus, m);
+     return l_linear.first;
+  }
 
-	for (int i=0; i<k; i++)
-		insert_in_list(&l_zyklus, 0, new IntListElem);
-		l_zyklus_last->next = l_zyklus.first;
+  if (n==0&&k!=0)
+  {
+     for (int j=0; j<k; j++)
+       insert_in_list(&l_linear, 0, new IntListElem);
+     return l_linear.first;
+  }
 
-	for (int j=0; j<n; j++)
-		insert_in_list(&l_linear, 0, new IntListElem);
+  if (n!=0&&k!=0)
+  {
+     IntListElem* l_zyklus_last = new IntListElem;
+     IntListElem* l_linear_last = new IntListElem;
+     insert_in_list(&l_zyklus, 0, l_zyklus_last);
+     insert_in_list(&l_linear, 0, l_linear_last);
 
-	l_linear_last->next = durchlaufe_liste(l_zyklus, m);
+     for (int i=1; i<n; i++)
+       insert_in_list(&l_zyklus, 0, new IntListElem);
+     l_zyklus_last->next = l_zyklus.first;
 
-	return &l_linear;
+     for (int j=1; j<k; j++)
+       insert_in_list(&l_linear, 0, new IntListElem);
+     l_linear_last->next = durchlaufe_liste(l_zyklus, m);
+
+     return l_linear.first;
+  }
 }
 
 IntListElem* laufen(IntListElem* elem, int schritte)
 {
-	IntListElem* elem_h = elem;
-	for (int i=0; i<schritte; i++)
-		elem_h = elem_h->next;
-	return elem_h;
+  IntListElem* elem_h = elem;
+  for (int i=0; i<schritte; i++)
+    elem_h = elem_h->next;
+  return elem_h;
 }
 
 int zykluslaenge(IntList rho)
 {
-	IntListElem* hase = rho.first;
-	IntListElem* igel = rho.first;
+  IntListElem* hase = rho.first;
+  IntListElem* igel = rho.first;
+  do
+  {
+   igel = laufen(igel, 1);
+   hase = laufen(hase, 2);
+  }
+  while (igel != hase || igel!=0 || hase!=0);
 
-	do {
-		igel = laufen(igel, 1);
-		hase = laufen(hase, 2);
-	} while (igel != hase);
-
-	int i=0;
-	do {
-		igel = laufen(igel, 1);
-		hase = laufen(hase, 2);
-		i++;
-	} while (igel != hase);
-
-	return i;
+  if (igel==0||hase==0)
+    return 0;
+  if (igel!=hase)
+  {
+   int i=0;
+   do
+   {
+    igel = laufen(igel, 1);
+    hase = laufen(hase, 2);
+    i++;
+   }
+   while (igel != hase);
+   return i;
+  }
 }
 int main()
 {
+  IntList s;
+  s.first= ZyklusListe(1, 3, 2);
+  print("n=",zykluslaenge(s));
 
+  return 0;
 }
+
+
+
+
